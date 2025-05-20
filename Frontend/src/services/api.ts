a1b04fa51871c3675fb1903d4ebd3db889e06122
ELMOURABETNADA <nadaelmourabet2002@gmail.com>
2025-05-20 17:02:25 +0100

    Partie admin

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -8,37 +8,37 @@ const loginAdmin = async (email: string, password: string) => {
 };
 
 const getReservations = async () => {
-  const response = await axios.get(`${API_URL}/reservations`);
+  const response = await axios.get(`${API_URL}/admin/reservations`);
   return response.data;
 };
 
 const getClients = async () => {
-  const response = await axios.get(`${API_URL}/clients`);
+  const response = await axios.get(`${API_URL}/admin/clients`);
   return response.data;
 };
 
 const getManagers = async () => {
-  const response = await axios.get(`${API_URL}/managers`);
+  const response = await axios.get(`${API_URL}/admin/managers`);
   return response.data;
 };
 
 const addManager = async (manager: any) => {
-  const response = await axios.post(`${API_URL}/managers`, manager);
+  const response = await axios.post(`${API_URL}/admin/managers`, manager);
   return response.data;
 };
 
 const updateManager = async (id: string, manager: any) => {
-  const response = await axios.put(`${API_URL}/managers/${id}`, manager);
+  const response = await axios.put(`${API_URL}/admin/managers/${id}`, manager);
   return response.data;
 };
 
 const deleteManager = async (id: string) => {
-  const response = await axios.delete(`${API_URL}/managers/${id}`);
+  const response = await axios.delete(`${API_URL}/admin/managers/${id}`);
   return response.data;
 };
 
 const updateReservationStatus = async (reservationId, status) => {
-  const response = await axios.put(`${API_URL}/reservations/${reservationId}`, {
+  const response = await axios.put(`${API_URL}/admin/reservations/${reservationId}`, {
     statut: status
   });
   return response.data;
